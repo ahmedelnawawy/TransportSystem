4db5af7d46d7ab95bb4fbecf8b084e4bc385bfe4
ahmedelnawawy <ahmedelnawawy978@gmail.com>
2020-03-02 13:31:01 +0200

    commit Ahmed

--- TransportSystems/Views/Reports/ItemsCard.aspx.cs
+++ TransportSystems/Views/Reports/ItemsCard.aspx.cs
@@ -15,7 +15,7 @@ namespace TransportSystems.Views.Reports
     {      
         ConnectDataBase.FillFunction fun1 = new ConnectDataBase.FillFunction();
         ConnectFunction fun = new ConnectFunction();
-        public string Test = "Ayman abdelkaer"; 
+        public string Test = "Ahmed zemel Ayman abdelkaer"; 
 
 
         InventoryReports.CardOfCategory coc = new AdsSysWeb.Models.LmsaEntitiesDB.InventoryReports.CardOfCategory();
